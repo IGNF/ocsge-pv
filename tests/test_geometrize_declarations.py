8b29920b23b0d26838ba463e3c89fc8349d864a4
Xavier Dumetz <xavier.dumetz@ign.fr>
2025-01-09 17:44:35 +0100

    WIP: resources, fixtures, folder structure.

---
+++ tests/test_geometrize_declarations.py
@@ -0,0 +1,12 @@
+"""Describes unit tests for the ocsge_pv.geometrize_declarations module.
+
+There is one test class for each tested functionnality.
+See internal docstrings for more information.
+Each variable prefixed by "m_" is a mock, or part of it.
+"""
+
+from unittest import TestCase, mock
+from unittest.mock import MagicMock, call, mock_open
+
+import pytest
+
