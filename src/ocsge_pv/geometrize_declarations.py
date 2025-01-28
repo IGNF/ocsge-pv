b112bff17ddb3e5bb0f80c832edfd1bb44bca47d
Xavier Dumetz <xavier.dumetz@ign.fr>
2025-01-28 16:34:45 +0100

    WIP: main function for geometrize_declarations Specifically: OGR/OSR entities creation.

--- src/ocsge_pv/geometrize_declarations.py
+++ src/ocsge_pv/geometrize_declarations.py
@@ -3,6 +3,7 @@
 
 # -- IMPORTS --
 # standard library
+from copy import deepcopy
 from datetime import date, datetime
 import json
 import os
@@ -39,12 +40,40 @@ def load_configuration(path: str) -> Dict:
     validation_schema_path = "src/ocsge_pv/resources/geometrize_config.schema.json"
     with open(path, "r", encoding="utf-8") as config_file:
         config_str = config_file.read()
-    configuration = json.loads(config_str)
+    source_configuration = json.loads(config_str)
     with open(validation_schema_path, "r", encoding="utf-8") as schema_file:
         schema_str = schema_file.read()
     schema = json.loads(schema_str)
-    jsonschema.validate(configuration, schema)
-    return configuration
+    jsonschema.validate(source_configuration, schema)
+    modified_configuration = deepcopy(source_configuration)
+    # Declarations data (input + output)
+    modified_configuration["main_database"]["_pg_string"] = ("host=" + modified_configuration['main_database']['host']
+        + " port=" + str(modified_configuration['main_database']['port'])
+        + " dbname=" + modified_configuration['main_database']['name']
+        + " user=" + modified_configuration['main_database']['user']
+        + " password=" + modified_configuration['main_database']['password'])
+    modified_configuration["main_database"]["_pg_string"] = ("host=" + modified_configuration['main_database']['host']
+        + " port=" + str(modified_configuration['main_database']['port'])
+        + " dbname=" + modified_configuration['main_database']['name']
+        + " user=" + modified_configuration['main_database']['user']
+        + " password=" + modified_configuration['main_database']['password'])
+    modified_configuration["main_database"]["_table_name_raw"] = (
+        modified_configuration["main_database"]["schema"] + "."
+        + modified_configuration["main_database"]["table"])
+    modified_configuration["main_database"]["_table_name_sql"] = sql.SQL(".").join([
+        modified_configuration["main_database"]["schema"],
+        modified_configuration["main_database"]["table"]])
+    # Cadastral data (input)
+    modified_configuration["cadastre_database"]["_pg_string"] = (
+        "host=" + modified_configuration["cadastre_database"]["host"]
+        + " port=" + str(modified_configuration["cadastre_database"]["port"])
+        + " dbname=" + modified_configuration["cadastre_database"]["name"]
+        + " user=" + modified_configuration["cadastre_database"]["user"]
+        + " password=" + modified_configuration["cadastre_database"]["password"])
+    modified_configuration["cadastre_database"]["_table_name_raw"] = (
+        modified_configuration["cadastre_database"]["schema"] + "."
+        + modified_configuration["cadastre_database"]["table"])
+    return modified_configuration
 
 
 # -- MAIN FUNCTION --
@@ -54,13 +83,38 @@ def main(configuration_file_path: str) -> None:
     Args:
         configuration_file_path (str): path to the configuration file
     """
+    # Read configuration
     configuration = load_configuration(configuration_file_path)
-    main_db_connection_string=("host=" + configuration['main_database']['host']
-        + " port=" + str(configuration['main_database']['port'])
-        + " dbname=" + configuration['main_database']['name']
-        + " user=" + configuration['main_database']['user']
-        + " password=" + configuration['main_database']['password'])
-    with psycopg.connect(main_db_connection_string) as conn:
+    # Connect to OGR datasources
+    declaration_ogr_ds = ogr.Open(configuration["main_database"]["_pg_string"])
+    cadastre_ogr_ds = ogr.Open(configuration["cadastre_database"]["_pg_string"])
+    # Compute SRS and conversions
+    ## Declarations layer's SRS
+    declaration_ogr_layer = declaration_ogr_ds.GetLayerByName(
+        configuration["main_database"]["_table_name_raw"])
+    declaration_ogr_srs = declaration_ogr_layer.GetSpatialRef()
+    if (declaration_ogr_srs is None):
+        raise ValueError("Declarations layer's SRS not found.")
+    ## Cadastre layer's SRS
+    cadastre_ogr_layer = cadastre_ogr_ds.GetLayerByName(
+        configuration["cadastre_database"]["_table_name_raw"])
+    cadastre_ogr_srs = cadastre_ogr_layer.GetSpatialRef()
+    if (cadastre_ogr_srs is None):
+        raise ValueError("Cadastre layer's SRS not found.")
+    ## OGR coordinates transformation, from cadastre to declarations
+    ogr_ct = None
+    coordinates_need_swap = False
+    latlon_sr_name_list = ['WGS 84']
+    if (cadastre_ogr_srs != declaration_ogr_srs):
+        ogr_ct = osr.CreateCoordinateTransformation(cadastre_ogr_srs, declaration_ogr_srs)
+        is_cadastre_srs_latlon = (cadastre_ogr_srs.EPSGTreatsAsLatLong() or cadastre_ogr_srs.GetName() in latlon_sr_name_list)
+        is_declaration_srs_latlon = (declaration_ogr_srs.EPSGTreatsAsLatLong() or declaration_ogr_srs.GetName() in latlon_sr_name_list)
+        coordinates_need_swap = (
+            (is_cadastre_srs_latlon and not is_declaration_srs_latlon)
+            or (is_declaration_srs_latlon and not is_cadastre_srs_latlon)
+        )
+    # Write output
+    with psycopg.connect(configuration["main_database"]["_pg_string"]) as conn:
         with conn.cursor() as cur:
             print()
 
--- tests/test_geometrize_declarations.py
+++ tests/test_geometrize_declarations.py
@@ -5,12 +5,13 @@ See internal docstrings for more information.
 Each variable prefixed by "m_" is a mock, or part of it.
 Each variable prefixed by "f_" is a fixture.
 """
-
+from copy import deepcopy
 import json
 from unittest import TestCase, mock, skip
-from unittest.mock import Mock, MagicMock, call, mock_open, patch
+from unittest.mock import MagicMock, call, mock_open, patch
 
 from jsonschema import validate, ValidationError
+from psycopg import sql
 import pytest
 
 from ocsge_pv.geometrize_declarations import (
@@ -31,14 +32,19 @@ class TestConfigurationValidationSchema(TestCase):
             self.schema = json.load(fp)
 
     def test_with_valid_config(self):
+        # Preparation
         with open(self.f_config_ok_path, "r", encoding="utf-8") as fp:
             f_config_obj = json.load(fp)
+        # Call to the tested function
         result = validate(f_config_obj, self.schema)
+        # Assertions
         self.assertIsNone(result)
 
     def test_with_invalid_config(self):
+        # Preparation
         with open(self.f_config_nok_path, "r", encoding="utf-8") as fp:
             f_config_obj = json.load(fp)
+        # Call to the tested function (while asserting Exception)
         with self.assertRaises(ValidationError):
             validate(f_config_obj, self.schema)
 
@@ -75,28 +81,56 @@ class TestConfigurationLoader(TestCase):
     @patch("jsonschema.validate")
     @patch("builtins.open")
     def test_load_configuration_ok(self, m_open, m_validator):
+        # Preparation
         m_open.side_effect = [
             mock_open(read_data=self.f_config_ok_raw).return_value,
             mock_open(read_data=self.f_config_schema_raw).return_value
         ]
+        expected_result = deepcopy(self.f_config_ok_obj)
+        expected_result["main_database"]["_pg_string"] = (
+            "host=" + expected_result["main_database"]["host"]
+            + " port=" + str(expected_result["main_database"]["port"])
+            + " dbname=" + expected_result["main_database"]["name"]
+            + " user=" + expected_result["main_database"]["user"]
+            + " password=" + expected_result["main_database"]["password"])
+        expected_result["main_database"]["_table_name_raw"] = (
+            expected_result["main_database"]["schema"] + "."
+            + expected_result["main_database"]["table"])
+        expected_result["main_database"]["_table_name_sql"] = sql.SQL(".").join([
+            expected_result["main_database"]["schema"],
+            expected_result["main_database"]["table"]])
+        expected_result["cadastre_database"]["_pg_string"] = (
+            "host=" + expected_result["cadastre_database"]["host"]
+            + " port=" + str(expected_result["cadastre_database"]["port"])
+            + " dbname=" + expected_result["cadastre_database"]["name"]
+            + " user=" + expected_result["cadastre_database"]["user"]
+            + " password=" + expected_result["cadastre_database"]["password"])
+        expected_result["cadastre_database"]["_table_name_raw"] = (
+            expected_result["cadastre_database"]["schema"] + "."
+            + expected_result["cadastre_database"]["table"])
+        # Call to the tested function
         result = load_configuration(self.f_config_ok_path)
+        # Assertions
         m_open.assert_called()
         m_open.assert_has_calls([
             call(self.f_config_ok_path, "r", encoding="utf-8"),
             call(self.f_config_schema_path, "r", encoding="utf-8")
         ])
         m_validator.assert_called_with(self.f_config_ok_obj, self.f_config_schema_obj)
-        assert result == self.f_config_ok_obj
+        self.assertDictEqual(result, expected_result)
 
     @patch("jsonschema.validate", side_effect=ValidationError("Invalid configuration."))
     @patch("builtins.open")
-    def test_load_configuration_ok(self, m_open, m_validator):
+    def test_load_configuration_nok(self, m_open, m_validator):
+        # Preparation
         m_open.side_effect = [
             mock_open(read_data=self.f_config_nok_raw).return_value,
             mock_open(read_data=self.f_config_schema_raw).return_value
         ]
+        # Call to the tested function (while asserting Exception)
         with self.assertRaises(ValidationError):
             result = load_configuration(self.f_config_nok_path)
+        # Assertions
         m_open.assert_called()
         m_open.assert_has_calls([
             call(self.f_config_nok_path, "r", encoding="utf-8"),
@@ -119,28 +153,83 @@ class TestMain(TestCase):
     """Tests the main routine, entrypoint for the executable."""
     def setUp(self):
         self.m_execute = MagicMock()
-        self.m_cursor = MagicMock(name="psycopg.Cursor")
+        self.m_cursor = MagicMock()
         self.m_cursor.__enter__.return_value.execute = self.m_execute
 
+    @patch("osgeo.osr.CreateCoordinateTransformation")
+    @patch("osgeo.ogr.Open")
     @patch("psycopg.connect")
     @patch("ocsge_pv.geometrize_declarations.load_configuration")
-    def test_ok(self, m_loader, m_psycopg_connect):
+    def test_ok(self, m_loader, m_psycopg_connect, m_ogr_open, m_osr_createct):
+        # Preparation
+        ## OGR/OSR entities 
+        m_main_ogr_ds = MagicMock()
+        m_main_ogr_lay = MagicMock()
+        m_main_ogr_sr = MagicMock()
+        m_main_ogr_lay.GetSpatialRef.return_value = m_main_ogr_sr
+        m_main_ogr_ds.GetLayerByName.return_value = m_main_ogr_lay
+        m_main_ogr_sr.GetName.return_value = "RGF93 v1 / Lambert-93"
+        m_main_ogr_sr.EPSGTreatsAsLatLong.return_value = None
+        m_parcel_ogr_ds = MagicMock()
+        m_parcel_ogr_lay = MagicMock()
+        m_parcel_ogr_sr = MagicMock()
+        m_parcel_ogr_lay.GetSpatialRef.return_value = m_parcel_ogr_sr
+        m_parcel_ogr_ds.GetLayerByName.return_value = m_parcel_ogr_lay
+        m_parcel_ogr_sr.GetName.return_value = "WGS 84"
+        m_parcel_ogr_sr.EPSGTreatsAsLatLong.return_value = None
+        m_ogr_open.side_effect=[m_main_ogr_ds, m_parcel_ogr_ds]
         m_psycopg_connect.return_value.__enter__.return_value.cursor = self.m_cursor
+        ## Configuration object
         f_config_path = "tests/fixtures/geometrize_config.ok.json"
         with open(f_config_path, "r", encoding="utf-8") as fp:
             f_configuration = json.load(fp)
+        f_configuration["main_database"]["_pg_string"] = (
+            "host=" + f_configuration["main_database"]["host"]
+            + " port=" + str(f_configuration["main_database"]["port"])
+            + " dbname=" + f_configuration["main_database"]["name"]
+            + " user=" + f_configuration["main_database"]["user"]
+            + " password=" + f_configuration["main_database"]["password"])
+        f_configuration["cadastre_database"]["_pg_string"] = (
+            "host=" + f_configuration["cadastre_database"]["host"]
+            + " port=" + str(f_configuration["cadastre_database"]["port"])
+            + " dbname=" + f_configuration["cadastre_database"]["name"]
+            + " user=" + f_configuration["cadastre_database"]["user"]
+            + " password=" + f_configuration["cadastre_database"]["password"])
+        f_configuration["main_database"]["_table_name_raw"] = (
+            f_configuration["main_database"]["schema"] + "."
+            + f_configuration["main_database"]["table"])
+        f_configuration["main_database"]["_table_name_sql"] = ("SQL("
+            + f_configuration["main_database"]["_table_name_raw"] + ")")
+        f_configuration["cadastre_database"]["_table_name_raw"] = (
+            f_configuration["cadastre_database"]["schema"] + "."
+            + f_configuration["cadastre_database"]["table"])
         m_loader.return_value = f_configuration
+        # Call to the tested function
         main(f_config_path)
+        # Assertions
+        ## Configuration
         m_loader.assert_called_once_with(f_config_path)
-        main_db_connection_string=("host=" + f_configuration['main_database']['host']
-            + " port=" + str(f_configuration['main_database']['port'])
-            + " dbname=" + f_configuration['main_database']['name']
-            + " user=" + f_configuration['main_database']['user']
-            + " password=" + f_configuration['main_database']['password'])
+        ## OGR/OSR
+        m_ogr_open.assert_called()
+        self.assertEqual(m_ogr_open.call_count, 2)
+        m_main_ogr_ds.GetLayerByName.assert_called_once_with(
+            f_configuration["main_database"]["_table_name_raw"])
+        m_main_ogr_lay.GetSpatialRef.assert_called_once()
+        m_main_ogr_sr.GetName.assert_called_once()
+        m_main_ogr_sr.EPSGTreatsAsLatLong.assert_called_once()
+        m_parcel_ogr_ds.GetLayerByName.assert_called_once_with(
+            f_configuration["cadastre_database"]["_table_name_raw"])
+        m_parcel_ogr_lay.GetSpatialRef.assert_called_once()
+        m_parcel_ogr_sr.GetName.assert_called_once()
+        m_parcel_ogr_sr.EPSGTreatsAsLatLong.assert_called_once()
+        m_osr_createct.assert_called_once_with(m_parcel_ogr_sr, m_main_ogr_sr)
+        ## Psycopg
         m_psycopg_connect.assert_has_calls([
-            call(main_db_connection_string)
+            call(f_configuration["main_database"]["_pg_string"])
         ])
         self.m_cursor.assert_called()
+        self.m_execute.assert_called()
+        self.assertIn(call("SELECT "), self.m_execute.call_args_list)
 
 
 
