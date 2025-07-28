27f278956a0d9b9ba602c29517c47484e31c0685
Xavier Dumetz <xavier.dumetz@ign.fr>
2025-07-28 19:43:32 +0200

    Splitting the SQL initialisation scripts to account for separate schemas.

--- src/ocsge_pv/resources/init_aggregation_db_schema.sql
+++ src/ocsge_pv/resources/init_aggregation_db_schema.sql
@@ -1,49 +1,5 @@
--- Minimal requirements for tables used in this project
+-- Minimal requirements for tables used in this project's pairing part
 -- Include the definition of the view for web services
-
--- Declarations as extracted and archived from the source web service
--- Some columns are used only for filtering exports
-CREATE TABLE IF NOT EXISTS "raw_dossier" (
-    "id_dossier" bigint PRIMARY KEY,
-    "porteur" bool,
-    "siret_port" char(14),
-    "ref_urba" text,
-    "type_proj" text,
-    "surf_socle" decimal(17, 4),
-    "etat" text,
-    "puiss_max" int,
-    "date_depot" date,
-    "date_deliv" date,
-    "date_insta" date,
-    "duree_exp" int,
-    "adresse" text,
-    "num_parcelles" text,
-    "surf_occup" decimal(17, 4),
-    "surf_terr" decimal(17, 4),
-    "localisat" text,
-    "sol_nature" text,
-    "sol_detail" text,
-    "usage_terr" text,
-    "type_agri" text,
-    "agri_ini" text,
-    "agri_resid" text,
-    "ancrage" text,
-    "cloture" text,
-    "revetement" text,
-    "haut_pann" decimal(6, 3),
-    "espacement" decimal(8, 3),
-    "nat_pieux" bool,
-    "transit" bool,
-    "agrivolt" bool,
-    "ex_date" bool,
-    "ex_agriv" bool,
-    "ex_techniq" bool,
-    "creation" timestamp (0) with time zone,
-    "geom" geometry(GEOMETRYCOLLECTION,2154),
-    "dern_modif" timestamp (0) with time zone,
-    "archive" bool,
-    "supprime" bool
-);
 
 -- Declarations as used by the pairing tool
 CREATE TABLE IF NOT EXISTS "declaration" (
@@ -87,7 +43,7 @@ CREATE TABLE IF NOT EXISTS "declaration" (
 
 -- Detections as used by the pairing tool
 CREATE TABLE IF NOT EXISTS "detection" (
-    "id_v2" bigint PRIMARY KEY,
+    "id" bigint PRIMARY KEY,
     "long" decimal(11, 8),
     "lat" decimal(11, 8),
     "surf_parc" decimal(17, 4),
@@ -96,7 +52,7 @@ CREATE TABLE IF NOT EXISTS "detection" (
     "insee_com" text,
     "nom_com" text,
     "millesime" int,
-    "geom" geometry(MULTIPOLYGON,2154)
+    "geom" geometry(POLYGON,2154)
 );
 
 -- Pairs as created by the pairing tool
@@ -109,7 +65,7 @@ CREATE UNIQUE INDEX IF NOT EXISTS "declaration_detection_idx" ON "declaration_de
 -- View used as the underlying data for public diffusion
 CREATE OR REPLACE VIEW "donnees_agregees" AS
     SELECT
-        "detection"."id_v2" AS "id",
+        "detection"."id",
         "detection"."long",
         "detection"."lat",
         "detection"."surf_parc",
@@ -150,5 +106,5 @@ CREATE OR REPLACE VIEW "donnees_agregees" AS
         INNER JOIN "declaration_detection"
         ON "declaration"."id_dossier"="declaration_detection"."declaration_id"
         RIGHT JOIN "detection"
-        ON "declaration_detection"."detection_id"="detection"."id_v2"
+        ON "declaration_detection"."detection_id"="detection"."id"
 ;
